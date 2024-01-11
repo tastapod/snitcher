e01e33206de4179e2a801bb712e99e81d7febb66
delano <delano@cpan.org>
2024-01-11 14:07:12 -0800

    Update snitcher script compatibility
    - Adds OS detection to snitcher.sh for cross-platform compatibility when splitting files:
        - Uses 'gsplit' on macOS if available (can be installed via coreutils)
        - Otherwise uses the macos version of 'split' with equivalent arguments.

--- snitcher.sh
+++ snitcher.sh
@@ -6,6 +6,7 @@ set -e # Bail if anything goes wrong
 URL="${URL:-https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts}"
 NAME="${NAME:-Steven Black}"
 PREFIX="${PREFIX:-unified}"
+OS="${OS:-$(uname)}"
 
 working_file="$PREFIX.working"
 
@@ -17,7 +18,30 @@ local now="$(date -Iseconds -u) UTC"
 curl --silent "$URL" \
     | awk '$1 == "0.0.0.0" && $2 != "0.0.0.0" {printf "    \"%s\",\n", $2}' \
     | sort > "$working_file"
-split -d --number l/2 "$working_file" part-
+
+# Split the file in half to avoid Little Snitch rule group
+# size limit of 200k entries.
+if [ "$OS" = "Darwin" ]; then
+
+    # On macOS, check if 'gsplit' is available so that we can
+    # use the same arguments as Linux. Otherwise, use the
+    # macos version of 'split' which has different arguments
+    # but produces a similar result.
+    if command -v gsplit2 > /dev/null; then
+        gsplit -d --number l/2 "$working_file" part-
+
+    else
+        # The macos version of 'split' takes different arguments
+        # but still produces two files of roughly equal size. The
+        # only difference is that the suffix of the files starts
+        # with '-aa' instead of '-00').
+        split -l $(expr $(wc -l < "$working_file") / 2 + 1) "$working_file" part-
+    fi
+else
+    # On other systems, use 'split'
+    split -d --number l/2 "$working_file" part-
+fi
+
 rm "$working_file"
 
 for part in part-*; do
