32fc6376ce5821fbcc70428b332eb797540a44c5
Daniel Terhorst-North <daniel@dannorth.net>
2025-02-17 17:12:12 +0000

    can't use local in top level of bash script

--- snitcher.sh
+++ snitcher.sh
@@ -10,20 +10,20 @@ PREFIX="${PREFIX:-unified}"
 working_file="$PREFIX.working"
 
 # yyyy-mm-dd
-local now="$(date -Iseconds -u) UTC"
+now="$(date -Iseconds -u) UTC"
 
 # Fetch the entries, filter the good ones, chunk into 200k pieces
 # due to a limitation in Little Snitch rule group size
 curl --silent "$URL" \
     | awk '$1 == "0.0.0.0" && $2 != "0.0.0.0" {printf "    \"%s\",\n", $2}' \
     | sort > "$working_file"
-local chunk_size=$(( ($(wc -l < "$working_file") + 1) / 2))
+chunk_size=$(( ($(wc -l < "$working_file") + 1) / 2))
 split -d -l $chunk_size "$working_file" part-
 rm "$working_file"
 
 for part in part-*; do
-  local part_num="${part#part-}"
-  local target="$PREFIX-$part_num.lsrules"
+  part_num="${part#part-}"
+  target="$PREFIX-$part_num.lsrules"
 
   # TODO: replace this with sed -i
   hosts=$(< $part)
